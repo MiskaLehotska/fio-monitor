858a6f7d020f52674651600cc9f606dc8ab921ec
Miska Lehotska <misela.le@gmail.com>
2022-12-17 19:29:47 +0100

    deleted unneccessary classes

--- src/main/java/sk/learning/fio/HTTP/FioService.java
+++
@@ -1,7 +0,0 @@
-package sk.learning.fio.HTTP;
-
-//this class will hold service methods about fio 
-
-public class FioService {
-
-}
--- src/main/java/sk/learning/fio/HTTP/TelegramService.java
+++
@@ -1,7 +0,0 @@
-package sk.learning.fio.HTTP;
-
-//this class will call methods of HTTP sluzby telegram api
-
-public class TelegramService {
-
-}
